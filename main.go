4ba53e2d3680faac92b76e0fb7a1ebd32513aaf3
Daniel Munoz <danielm@remitly.com>
2022-08-24 16:49:38 -0700

    Moved ui code into proper package

--- main.go
+++ main.go
@@ -3,13 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
-	"atomicgo.dev/cursor"
-	"github.com/daniel-munoz/life/keyboard"
 	"github.com/daniel-munoz/life/model"
+	"github.com/daniel-munoz/life/ui"
 )
 
 func main() {
@@ -36,93 +32,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	display(w, -10, -10, 40, 80)
-}
-
-func display(w model.World, top, left, bottom, right int64) {
-	stopChannel := make(chan struct{})
-	sigs := make(chan os.Signal, 1)
-
-	signal.Notify(sigs, syscall.SIGTERM)
-
-	cursor.Hide()
-	defer cursor.Show()
-
-	area := cursor.NewArea()
-
-	listener := keyboard.NewListener()
-	listener.Start()
-	helpTimer := 8
-
-	paused := false
-	go func() {
-		for {
-			time.Sleep(250 * time.Millisecond)
-			if helpTimer > 0 {
-				area.Update(`Keys:
-  Up   : moves window 1 space up       Down : moves window 1 space down
-  Left : moves window 1 space left     Right: moves window 1 space right
-  I    : moves window 10 spaces up     K    : moves window 10 spaces down
-  J    : moves window 10 spaces left   L    : moves window 10 spaces right
-  Q    : ends the program              H    : displays this help
-  `)
-				helpTimer--
-				continue
-			}
-			if !paused {
-				w.Evolve()
-			}
-
-			topLeft, bottomRight := model.NewIndex(left,top), model.NewIndex(right,bottom)
-			area.Update(w.WindowContent(topLeft, bottomRight))
-
-			check := listener.Check()
-			switch(check) {
-			case keyboard.Stop:
-				stopChannel <- struct{}{}
-			case keyboard.Up:
-				top--
-				bottom--
-			case keyboard.Down:
-				top++
-				bottom++
-			case keyboard.Left:
-				left--
-				right--
-			case keyboard.Right:
-				left++
-				right++
-			case keyboard.PageUp:
-				top-=10
-				bottom-=10
-			case keyboard.PageDown:
-				top+=10
-				bottom+=10
-			case keyboard.PageLeft:
-				left-=10
-				right-=10
-			case keyboard.PageRight:
-				left+=10
-				right+=10
-			case keyboard.Help:
-				helpTimer = 8
-			case keyboard.Pause:
-				paused = !paused
-			default:
-			}
-		}
-	}()
-	for {
-		select {
-		case s := <-sigs:
-			switch s {
-			case syscall.SIGTERM:
-				area.Update("I've been told to stop")
-				return
-			}
-		case <-stopChannel:
-				area.Update("Time to stop")
-			return
-		}
-	}
+	ui.Show(w, -10, -10, 40, 80)
 }
---
+++ ui/legacy.go
@@ -0,0 +1,87 @@
+package ui 
+
+import (
+	"time"
+
+	"atomicgo.dev/cursor"
+	"github.com/daniel-munoz/life/event"
+	"github.com/daniel-munoz/life/model"
+)
+
+func Show(w model.World, top, left, bottom, right int64) {
+	stopChannel := make(chan struct{})
+
+	cursor.Hide()
+	defer cursor.Show()
+
+	display := NewDisplay()
+
+	listener := event.NewListener()
+	listener.Start()
+	showHelp := false
+
+	paused := false
+	go func() {
+		for {
+			if showHelp {
+				display.UpdateAndLock(`Keys:
+  Up   : moves window 1 space up       Down : moves window 1 space down
+  Left : moves window 1 space left     Right: moves window 1 space right
+  I    : moves window 10 spaces up     K    : moves window 10 spaces down
+  J    : moves window 10 spaces left   L    : moves window 10 spaces right
+  Q    : ends the program              H    : displays this help
+  `, 3500 * time.Millisecond)
+  				showHelp = false
+			}
+			if !paused {
+				w.Evolve()
+			}
+
+			topLeft, bottomRight := model.NewIndex(left,top), model.NewIndex(right,bottom)
+			display.UpdateAndLock(w.WindowContent(topLeft, bottomRight), 200 * time.Millisecond)
+
+			check := listener.Check()
+			switch(check) {
+			case event.Stop:
+				stopChannel <- struct{}{}
+				return
+			case event.Up:
+				top--
+				bottom--
+			case event.Down:
+				top++
+				bottom++
+			case event.Left:
+				left--
+				right--
+			case event.Right:
+				left++
+				right++
+			case event.PageUp:
+				top-=10
+				bottom-=10
+			case event.PageDown:
+				top+=10
+				bottom+=10
+			case event.PageLeft:
+				left-=10
+				right-=10
+			case event.PageRight:
+				left+=10
+				right+=10
+			case event.Help:
+				showHelp = true
+			case event.Pause:
+				paused = !paused
+			default:
+			}
+		}
+	}()
+	for {
+		select {
+		case <-stopChannel:
+				display.UpdateAndClose("Time to stop")
+			return
+		}
+	}
+}
